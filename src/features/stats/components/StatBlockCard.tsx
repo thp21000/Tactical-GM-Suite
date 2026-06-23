7f3ab658bf79ae9397c3b4ba7016d21898e6715f
GM Tools & Resources <fede.unl21@gmail.com>
2026-06-23 18:45:34 +0200

    Version 0.2.5

--- src/features/stats/components/StatBlockCard.tsx
+++ src/features/stats/components/StatBlockCard.tsx
@@ -1,15 +1,24 @@
-import type { StatDisplayGroup, StatTokenInput, StatTrackerInput } from "../statTypes";
+import type {
+  StatDisplayGroup,
+  StatTokenInput,
+  StatTrackerInput,
+} from "../statTypes";
 import { StatTrackedTokenBlock } from "./StatTrackedTokenBlock";
 
 type Props = {
   group: StatDisplayGroup;
   onAddTracker: (tokenId: string, input: StatTrackerInput) => void;
+  onApplyPreset: (tokenId: string) => void;
   onChangeTrackerValue: (tokenId: string, trackerId: string, delta: number) => void;
   onRemoveToken: (tokenId: string) => void;
   onRemoveTracker: (tokenId: string, trackerId: string) => void;
   onToggleTracker: (tokenId: string, trackerId: string) => void;
   onUpdateToken: (tokenId: string, input: Partial<StatTokenInput>) => void;
-  onUpdateTracker: (tokenId: string, trackerId: string, input: Partial<StatTrackerInput>) => void;
+  onUpdateTracker: (
+    tokenId: string,
+    trackerId: string,
+    input: Partial<StatTrackerInput>,
+  ) => void;
 };
 
 export function StatBlockCard(props: Props) {
