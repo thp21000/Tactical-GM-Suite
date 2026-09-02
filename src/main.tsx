65998cf9ad105ce250cb3921d9b0c2f7413b59ba
GM Tools & Resources <fede.unl21@gmail.com>
2026-09-02 09:57:22 +0200

    Style: charger les scrollbars intégrées globalement

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./shared/styles/globals.css";
+import "./shared/styles/scrollbars.css";
 import "./features/stats/statTrackerUi.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<React.StrictMode><App /></React.StrictMode>);
