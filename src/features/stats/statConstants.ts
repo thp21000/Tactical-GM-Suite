159ff8b406353ca755d9659c55377ec5f40f8428
GM Tools & Resources <fede.unl21@gmail.com>
2026-09-02 21:02:51 +0200

    Stats: ajouter l’identifiant du sous-menu de stats

--- src/features/stats/statConstants.ts
+++ src/features/stats/statConstants.ts
@@ -2,3 +2,4 @@ import { EXTENSION_ID } from "../../core/constants/ids";
 
 export const STAT_TRACKER_CONTEXT_MENU_ID = `${EXTENSION_ID}/add-to-stat-tracker`;
 export const STAT_CONDITION_CONTEXT_MENU_ID = `${EXTENSION_ID}/conditions`;
+export const STAT_STATS_CONTEXT_MENU_ID = `${EXTENSION_ID}/stats`;
