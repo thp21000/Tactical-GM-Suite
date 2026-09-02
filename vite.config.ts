a26821a96498387e76672c7569e0a46787e9f3ae
GM Tools & Resources <fede.unl21@gmail.com>
2026-09-02 13:09:40 +0200

    Stats: build dedicated background page

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/Tactical-GM-Suite/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        background: "background.html",
+      },
+    },
+  },
 });
