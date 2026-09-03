d1b6bdddd03327fdfbc6e376e19a3368e2b5da7c
GM Tools & Resources <fede.unl21@gmail.com>
2026-09-03 18:12:12 +0200

    I18n: ajouter les traductions Paramètres FR

---
+++ src/features/settings/i18n/fr.ts
@@ -0,0 +1,18 @@
+import type { LocaleDictionary } from "../../../i18n/types";
+
+const fr: LocaleDictionary = {
+  "settings.general.sectionTitle": "Langue / Système",
+  "settings.general.sectionSummary": "Préférences globales",
+  "settings.general.language.label": "Langue",
+  "settings.general.language.help": "La traduction de l’interface existante sera migrée progressivement. Les nouveaux écrans et les nouvelles fonctions utilisent déjà ce réglage.",
+  "settings.general.language.fr": "Français",
+  "settings.general.language.en": "English",
+  "settings.general.system.label": "Système de jeu",
+  "settings.general.system.help": "Les modules compatibles chargent le contenu correspondant au système sélectionné. Conditions utilise ce réglage dès maintenant.",
+  "settings.general.system.dnd5e": "D&D 5e",
+  "settings.general.system.pf2e": "Pathfinder 2e",
+  "settings.general.system.generic": "Générique",
+  "settings.general.system.genericHint": "Structure prête ; le contenu générique sera ajouté plus tard.",
+};
+
+export default fr;
