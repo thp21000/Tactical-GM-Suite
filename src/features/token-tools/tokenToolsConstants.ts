1ca3351ca58899471b93cc4624659b23f02f6b7f
GM Tools & Resources <fede.unl21@gmail.com>
2026-09-04 12:53:15 +0200

    Token tools: retirer l’ancien identifiant de popover

--- src/features/token-tools/tokenToolsConstants.ts
+++ src/features/token-tools/tokenToolsConstants.ts
@@ -1,4 +1,3 @@
 import { EXTENSION_ID } from "../../core/constants/ids";
 
 export const TOKEN_TOOLS_CONTEXT_MENU_ID = `${EXTENSION_ID}/token-tools`;
-export const TOKEN_TOOLS_POPOVER_ID = `${EXTENSION_ID}/token-tools-popover`;
